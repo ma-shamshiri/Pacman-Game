4667467864858e9a5e38d85b05f091f0c0c41af9
Mohammad Amin Shamshiri <41012199+ma-shamshiri@users.noreply.github.com>
2020-05-15 10:46:20 -0400

    Update search.py

--- Pacman/search.py
+++ Pacman/search.py
@@ -1,5 +1,6 @@
 # search.py
 # ---------
+# ---------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
